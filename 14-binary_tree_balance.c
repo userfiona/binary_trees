460d7ae40b26613eccd70830fea7cd5b167e81f5
userfiona <fionaramaesela@gmail.com>
2023-08-29 13:42:34 -0700

    update basic

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -37,5 +37,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
     height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
     height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-    return (height_l > height_r ? height_l : height_r);
+
+    return (height_l > height_r ? height_l : height_r) + 1;
 }
+
