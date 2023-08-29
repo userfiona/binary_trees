c9e5bace602ee5143e97f008f48765d0af842a37
userfiona <fionaramaesela@gmail.com>
2023-08-29 14:00:55 -0700

    update basic

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,15 +9,15 @@
  */
 size_t binary_tree_height_balance(const binary_tree_t *tree)
 {
-	size_t h_left = 0, h_right = 0;
+        size_t h_left = 0, h_right = 0;
 
-	if (!tree)
-		return 0;
+        if (!tree)
+                return (0);
 
-	h_left = tree->left ? 1 + binary_tree_height_balance(tree->left) : 0;
-	h_right = tree->right ? 1 + binary_tree_height_balance(tree->right) : 0;
+        h_left = tree->left ? 1 + binary_tree_height_balance(tree->left) : 0;
+        h_right = tree->right ? 1 + binary_tree_height_balance(tree->right) : 0;
 
-	return (h_left > h_right) ? h_left : h_right;
+        return ((h_left > h_right) ? h_left : h_right);
 }
 
 /**
@@ -29,14 +29,14 @@ size_t binary_tree_height_balance(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t h_left = 0, h_right = 0;
+        size_t h_left = 0, h_right = 0;
 
-	if (!tree)
-		return 0;
+        if (!tree)
+                return (0);
 
-	h_left = tree->left ? 1 + binary_tree_height_balance(tree->left) : 0;
-	h_right = tree->right ? 1 + binary_tree_height_balance(tree->right) : 0;
+        h_left = tree->left ? 1 + binary_tree_height_balance(tree->left) : 0;
+        h_right = tree->right ? 1 + binary_tree_height_balance(tree->right) : 0;
 
-	return h_left - h_right;
+        return (h_left - h_right);
 }
 
