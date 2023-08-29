771a41e9191e404d1a06b340c8b135028a59c24d
userfiona <fionaramaesela@gmail.com>
2023-08-29 12:31:52 -0700

    update basic

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,7 +11,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-    	binary_tree_t *new_node = NULL;
 
     if (new_node == NULL)
         return (NULL);
