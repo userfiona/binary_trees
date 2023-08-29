4d471f002340d9055862e33a5580d7feb853bbba
userfiona <fionaramaesela@gmail.com>
2023-08-29 14:05:10 -0700

    update basic

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -5,19 +5,19 @@
  * @tree: pointer to the root node of the tree to measure the balance factor
  *
  * Return: height
- * If tree is NULL, return 0
+ *         If tree is NULL, return 0
  */
 size_t binary_tree_height_balance(const binary_tree_t *tree)
 {
-        size_t h_left = 0, h_right = 0;
+    size_t h_left = 0, h_right = 0;
 
-        if (!tree)
-                return (0);
+    if (!tree)
+        return (0);
 
-        h_left = tree->left ? 1 + binary_tree_height_balance(tree->left) : 0;
-        h_right = tree->right ? 1 + binary_tree_height_balance(tree->right) : 0;
+    h_left = tree->left ? 1 + binary_tree_height_balance(tree->left) : 0;
+    h_right = tree->right ? 1 + binary_tree_height_balance(tree->right) : 0;
 
-        return ((h_left > h_right) ? h_left : h_right);
+    return ((h_left > h_right) ? h_left : h_right);
 }
 
 /**
@@ -25,18 +25,17 @@ size_t binary_tree_height_balance(const binary_tree_t *tree)
  *
  * @tree: pointer to the root node of the tree
  * Return: balance factor
- * If tree is NULL, return 0
+ *         If tree is NULL, return 0
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-        size_t h_left = 0, h_right = 0;
+    size_t h_left = 0, h_right = 0;
 
-        if (!tree)
-                return (0);
+    if (!tree)
+        return (0);
 
-        h_left = tree->left ? 1 + binary_tree_height_balance(tree->left) : 0;
-        h_right = tree->right ? 1 + binary_tree_height_balance(tree->right) : 0;
+    h_left = tree->left ? 1 + binary_tree_height_balance(tree->left) : 0;
+    h_right = tree->right ? 1 + binary_tree_height_balance(tree->right) : 0;
 
-        return (h_left - h_right);
+    return (h_left - h_right);
 }
-
