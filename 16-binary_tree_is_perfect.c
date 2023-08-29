912268aafde1dcf7b5b5133faeda003314a8ef27
userfiona <fionaramaesela@gmail.com>
2023-08-29 15:01:25 -0700

    update basic

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,19 +9,20 @@
 int tree_is_perfect(const binary_tree_t *tree, int height)
 {
 	int left_height, right_height;
+	
 	if (!tree)
-		return height;
+		return (height);
 
 	if (!tree->left && !tree->right)
-		return height + 1;
+		return (height + 1);
 
 	left_height = tree_is_perfect(tree->left, height + 1);
 	right_height = tree_is_perfect(tree->right, height + 1);
 
 	if (left_height == -1 || right_height == -1 || left_height != right_height)
-		return -1;
+		return (-1);
 
-	return left_height;
+	return (left_height);
 }
 
 /**
@@ -32,6 +33,6 @@ int tree_is_perfect(const binary_tree_t *tree, int height)
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (!tree)
-		return 0;
+		return (0);
 	return (tree_is_perfect(tree, 0) != -1);
 }
