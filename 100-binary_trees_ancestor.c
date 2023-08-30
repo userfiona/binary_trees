81e42101907173e58e83ad3e599a4921ed9d3cd9
userfiona <fionaramaesela@gmail.com>
2023-08-30 00:28:17 -0700

    update basic

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,34 +9,34 @@
  *         NULL if there is no ancestor node
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-                                     const binary_tree_t *second)
+		const binary_tree_t *second)
 {
-    size_t depth_first, depth_second;
+	size_t depth_first, depth_second;
 
-    if (!first || !second)
-        return (NULL);
+	if (!first || !second)
+		return (NULL);
 
-    depth_first = binary_tree_depth(first);
-    depth_second = binary_tree_depth(second);
+	depth_first = binary_tree_depth(first);
+	depth_second = binary_tree_depth(second);
 
-    while (depth_first > depth_second)
-    {
-        first = first->parent;
-        depth_first--;
-    }
-    while (depth_second > depth_first)
-    {
-        second = second->parent;
-        depth_second--;
-    }
-    while (first && second)
-    {
-        if (first == second)
-            return ((binary_tree_t *)first);
-        first = first->parent;
-        second = second->parent;
-    }
-    return ((binary_tree_t *)first);
+	while (depth_first > depth_second)
+	{
+		first = first->parent;
+		depth_first--;
+	}
+	while (depth_second > depth_first)
+	{
+		second = second->parent;
+		depth_second--;
+	}
+	while (first && second)
+	{
+		if (first == second)
+			return ((binary_tree_t *)first);
+		first = first->parent;
+		second = second->parent;
+	}
+	return ((binary_tree_t *)first);
 }
 
 /**
@@ -48,16 +48,16 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    size_t depth = 0;
+	size_t depth = 0;
 
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    while (tree->parent)
-    {
-        depth++;
-        tree = tree->parent;
-    }
+	while (tree->parent)
+	{
+		depth++;
+		tree = tree->parent;
+	}
 
-    return (depth);
+	return (depth);
 }
