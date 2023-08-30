25768984469cb171fa162527362b1cb0d19e75de
userfiona <fionaramaesela@gmail.com>
2023-08-30 04:48:38 -0700

    update basic

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * heap_to_sorted_array - converts a Binary Max Heap to a sorted array of int
+ * heap_to_
+ * sorted_array - converts a Binary Max Heap to a sorted array of integers.
  *
  * @root: double pointer to the root node of heap
  * Return: value stored in the root node
