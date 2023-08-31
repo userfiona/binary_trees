4d22612b37b974f0f2f53a7e0f5bb00d49056f59
userfiona <fionaramaesela@gmail.com>
2023-08-31 12:08:16 -0700

    update basic

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -8,13 +8,13 @@
  */
 bst_t *bst_search(const bst_t *tree, int value)
 {
-    if (tree == NULL)
-        return (NULL);
+	if (tree == NULL)
+		return (NULL);
 
-    if (value == tree->n)
-        return ((bst_t *)tree);
-    else if (value < tree->n)
-        return (bst_search(tree->left, value));
-    else
-        return (bst_search(tree->right, value));
+	if (value == tree->n)
+		return ((bst_t *)tree);
+	else if (value < tree->n)
+		return (bst_search(tree->left, value));
+	else
+		return (bst_search(tree->right, value));
 }
