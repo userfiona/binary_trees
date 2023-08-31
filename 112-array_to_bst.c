a414aee583e9c71510d4e6b9b2d3c303d4d98548
userfiona <fionaramaesela@gmail.com>
2023-08-31 12:22:06 -0700

    update basic

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -9,23 +9,23 @@
  */
 bst_t *array_to_bst(int *array, size_t size)
 {
-    bst_t *tree = NULL;
-    size_t i, j;
+	bst_t *tree = NULL;
+	size_t i, j;
 
-    if (array == NULL || size == 0)
-        return (NULL);
+	if (array == NULL || size == 0)
+		return (NULL);
 
-    for (i = 0; i < size; i++)
-    {
-        if (bst_insert(&tree, array[i]) == NULL)
-        {
-            for (j = 0; j < size && array[j] != array[i]; j++)
-            {}
+	for (i = 0; i < size; i++)
+	{
+		if (bst_insert(&tree, array[i]) == NULL)
+		{
+			for (j = 0; j < size && array[j] != array[i]; j++)
+			{}
 
-            if (j == i)
-                return (NULL);
-        }
-    }
+			if (j == i)
+				return (NULL);
+		}
+	}
 
-    return (tree);
+	return (tree);
 }
