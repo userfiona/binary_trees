c7a36a00e39f3f0f6a9ebf20ed71cfd16bb2ea04
userfiona <fionaramaesela@gmail.com>
2023-08-31 15:36:17 -0700

    update basic

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,28 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * bst_t *bst_insert - Inserts a value in a Binary Search Tree.
- * @tree: Double pointer to the root node of the BST to insert the value.
- * @value: Value to store in the node to be inserted.
- * Return: Pointer to the created node, or NULL on failure.
+ * successor - Gets the next successor, which is the minimum node in the right subtree.
+ * @node: Tree to check.
+ * Return: The minimum value of this tree.
  */
-bst_t *bst_insert(bst_t **tree, int value)
+int successor(bst_t *node)
 {
-	if (!tree)
-		return NULL;
-
-	if (*tree == NULL)
-	{
-		*tree = binary_tree_node(NULL, value);
-		return *tree;
-	}
-
-	if (value < (*tree)->n)
-		return bst_insert(&((*tree)->left), value);
-	else if (value > (*tree)->n)
-		return bst_insert(&((*tree)->right), value);
-
-	return NULL;
+	if (node->left)
+		return successor(node->left);
+	return node->n;
 }
 
 /**
@@ -55,23 +42,9 @@ bst_t *bst_remove(bst_t *root, int value)
 			return temp;
 		}
 
-		bst_t *temp = binary_tree_min_value_node(root->right);
-		root->n = temp->n;
-		root->right = bst_remove(root->right, temp->n);
+		root->n = successor(root->right);
+		root->right = bst_remove(root->right, root->n);
 	}
 
 	return root;
-}
-
-/**
- * binary_tree_min_value_node - Finds the node with the minimum value in a BST.
- * @node: Pointer to the root node of the BST.
- * Return: Pointer to the node with the minimum value.
- */
-bst_t *binary_tree_min_value_node(bst_t *node)
-{
-	bst_t *current = node;
-	while (current && current->left)
-		current = current->left;
-	return current;
 }
