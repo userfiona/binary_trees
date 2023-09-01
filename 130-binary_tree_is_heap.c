4cea606f1a385464e4651f4051ec8684efe8b623
userfiona <fionaramaesela@gmail.com>
2023-08-31 17:17:19 -0700

    update basic

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include "binary_trees.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	binary_tree_t *root = ...; /* Initialize your binary tree here */
-	int is_heap = binary_tree_is_heap(root);
+    binary_tree_t *root = ...; /* Initialize your binary tree here */
+    int is_heap = binary_tree_is_heap(root);
 
-	if (is_heap)
-	{
-		printf("The tree is a valid Max Binary Heap.\n");
-	}
-	else
-	{
-		printf("The tree is not a valid Max Binary Heap.\n");
-	}
+    if (is_heap)
+    {
+        printf("The tree is a valid Max Binary Heap.\n");
+    }
+    else
+    {
+        printf("The tree is not a valid Max Binary Heap.\n");
+    }
 
-	return (0);
+    return (0);
 }
