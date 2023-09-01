1ac52640cc654e2dba9aba181007ecef4fbd90be
userfiona <fionaramaesela@gmail.com>
2023-08-31 17:34:15 -0700

    update basic

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -2,14 +2,12 @@
 
 /**
  * array_to_heap - builds a Max Binary Heap tree from an array
+ *
  * @array: pointer to the first element of the array to be converted
  * @size: number of elements in the array
- * Return: pointer to the root node of the created Binary Heap, or NULL on failure
+ * Return: pointer to the root node of the created Binary Heap,
+ * or NULL on failure
  */
-
-/* Helper function to build the Max Binary Heap from an array */
-void heapify_array(heap_t *node);
-
 heap_t *array_to_heap(int *array, size_t size)
 {
 	size_t i;
@@ -18,25 +16,9 @@ heap_t *array_to_heap(int *array, size_t size)
 	if (!array)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
-	{
-		root = heap_insert(&root, array[i]);
-		if (!root)
-			return (NULL);
-	}
-
-	heapify_array(root);
+	root = heap_insert(&root, array[0]);
+	for (i = 1; i < size; i++)
+		heap_insert(&root, array[i]);
 
 	return (root);
-}
-
-/* Helper function to heapify the entire tree */
-void heapify_array(heap_t *node)
-{
-	if (!node)
-		return;
-
-	heapify_array(node->left);
-	heapify_array(node->right);
-	heapify_down(node);
 }
